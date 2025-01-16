890d533e531376cb7953252796d56793ae4bd1b3
Suraj Somani <surajsomani2002@gmail.com>
2025-01-16 18:54:17 +0530

    image optimization and post list compeleted

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -6,7 +6,11 @@ const FeaturedPosts = () => {
       {/* first  */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* image */}
-        <Image src="featured1.jpeg" className="rounded-3xl object-cover" />
+        <Image
+          src="featured1.jpeg"
+          className="rounded-3xl object-cover"
+          w="895"
+        />
         {/* details  */}
         <div className="flex items-center gap-4">
           <h1 className="font-semibold lg:text-lg">01.</h1>
@@ -25,10 +29,13 @@ const FeaturedPosts = () => {
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* second  */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className=" w-1/3 aspect-video">
+            <Image
+              src="featured2.jpeg"
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and titles */}
           <div className="w-2/3">
             {/* details  */}
@@ -48,10 +55,13 @@ const FeaturedPosts = () => {
         </div>
         {/* third  */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className=" w-1/3 aspect-video">
+            <Image
+              src="featured2.jpeg"
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and titles */}
           <div className="w-2/3">
             {/* details  */}
@@ -71,10 +81,13 @@ const FeaturedPosts = () => {
         </div>
         {/* forth  */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className=" w-1/3 aspect-video">
+            <Image
+              src="featured2.jpeg"
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and titles */}
           <div className="w-2/3">
             {/* details  */}
--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import MainCategories from "../components/MainCategories";
 import FeaturedPosts from "../components/FeaturedPosts";
+import PostList from "../components/PostList";
 
 const Homepage = () => {
   return (
@@ -66,6 +67,10 @@ const Homepage = () => {
       {/* FEATURED POST  */}
       <FeaturedPosts />
       {/* POST LIST  */}
+      <div className="">
+        <h1 className="my-8 text-2xl text-gray-600">Recent Posts</h1>
+        <PostList />
+      </div>
     </div>
   );
 };
